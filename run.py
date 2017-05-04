5bababb51e97725587e60262ba7e2a67b6f9f2f9
Amadu <ndongamadou@gmail.com>
2017-05-04 13:15:05 +0000

    final

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
 df = pd.read_csv('data/COD-all.csv')
-datasets = df[['package_id','package_name','package_has_admin_boundary_tag','package_tag_list','package_has_cod_tag','package_author','package_caveats','package_dataset_date','package_total_res_downloads','package_methodology','package_title','package_creator','organisation_name','package_notes','package_num_resources','package_tracking_summary_total','package_dataset_source','package_locations_list']]
+datasets = df[['package_id','organisation_name','package_title','package_has_admin_boundary_tag','package_tag_list','package_has_cod_tag','package_author','package_caveats','package_dataset_date','package_total_res_downloads','package_methodology','package_creator','package_notes','package_num_resources','package_tracking_summary_total','package_dataset_source','package_locations_list']]
 
-dowloads = pd.read_csv('data/COD-Downloads.csv')
+downloads = pd.read_csv('data/COD-Downloads.csv')
+downloadStats = pd.merge(left=downloads, right=datasets, how='left', left_on='dataset_name', right_on='package_title')
+
 views = pd.read_csv('data/COD-pageviews.csv')
-stats = pd.merge(left=dowloads, right=views, how='left', on='dataset_name')
+viewStats = pd.merge(left=views, right=datasets, how='left', left_on='dataset_name', right_on='package_title')
 
-fullstats = pd.merge(left=stats, right=datasets, how='left', left_on='dataset_name', right_on='package_title')
-fullstats.to_csv('data/stats.csv')
 
-# df2 = [dowloads, views]
-# pagesStats = pd.concat(df2)
-# pagesStats.to_csv('data/stats.csv')
+downloadStats.to_csv('data/downloadStats.csv')
+viewStats.to_csv('data/viewStats.csv')
